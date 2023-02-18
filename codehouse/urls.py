051a1e8069eea376cd6a3740dd17a3ecb979eb52
Mayank Gupta <mayankgupta@Mayanks-MacBook-Pro.local>
2023-02-18 23:07:08 +0530

    Final fix of media issue

--- codehouse/urls.py
+++ codehouse/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib.auth import views as auth_views
 from authenticate.views import password_reset_request
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     path('', include('blog.urls')),
     #path('authenticate/', include('authenticate.urls')),
     #path('accounts/', include('django.contrib.auth.urls')),
