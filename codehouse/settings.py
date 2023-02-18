1139012332bc958cc2a64bf6b01d8b030793830d
Mayank Gupta <mayankgupta@Mayanks-MacBook-Pro.local>
2023-02-18 16:37:52 +0530

    Updated settings

--- codehouse/settings.py
+++ codehouse/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%n0tvt7!ymtvo493&mp*vda=cy!jil84-nen+^!rr7z+r$35bw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['writophobia.com', '127.0.0.1:8000']
 
 
 # Application definition
@@ -57,6 +57,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_TRUSTED_ORIGINS = ['https://*.writophobia.com', 'https://writophobia.com', 'http://127.0.0.1:8000']
+
 ROOT_URLCONF = 'codehouse.urls'
 
 TEMPLATES = [
@@ -81,12 +83,26 @@ WSGI_APPLICATION = 'codehouse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG == True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'writo',
+            'USER': 'mayank',
+            'PASSWORD': 'gt52kOnw',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
