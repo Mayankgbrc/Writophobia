11001bf5d99e7a3cc6877f72f2f5c2e357ad10f5
Mayank Gupta <mayankgupta@Mayanks-MacBook-Pro.local>
2023-03-08 13:53:04 +0530

    Added settings

--- codehouse/settings.py
+++ codehouse/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+GEOIP_PATH = os.path.join(BASE_DIR, 'geoip/')
 
 
 # Quick-start development settings - unsuitable for production
