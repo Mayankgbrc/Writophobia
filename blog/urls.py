560f940a4634bc16b4f0a1f485df3d3efec5c861
Mayank Gupta <mayankgupta@Mayanks-MacBook-Pro.local>
2023-04-02 02:14:19 +0530

    Added new urls

--- blog/urls.py
+++ blog/urls.py
@@ -6,11 +6,14 @@ from blog.views import (
     ContactView,
     APICreate
 )
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('view/<str:pk>/', BlogSelectedView.as_view(), name='blog-detail'),
+    path('profile/reetika_gautam/', TemplateView.as_view(template_name='custom/reetika.html'), name='reetika_profile'),
     path('profile/<str:username>/', ProfileView.as_view(), name='profile'),
     path('contact-us/', ContactView.as_view(), name='contact-us'),
     path('api_create/', APICreate.as_view()),
+    path('reetika_gautam/', TemplateView.as_view(template_name='custom/reetika.html')),
 ]
